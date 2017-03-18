f9bb9fa1a2378969f690d07a931ac3eac06c3740
Tran Tung <trantung04@gmail.com>
2017-03-18 20:49:18 +0700

    init

--- HelloWorldBot/Controllers/MessagesController.cs
+++ HelloWorldBot/Controllers/MessagesController.cs
@@ -1,5 +1,8 @@
-﻿using System.Net;
+﻿using System;
+using System.Globalization;
+using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.Bot.Builder.Dialogs;
@@ -16,18 +19,56 @@ namespace HelloWorldBot
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
+            // Global values
             if (activity.Type == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+                // Get any saved values
+                StateClient sc = activity.GetStateClient();
+                BotData userData = sc.BotState.GetPrivateConversationData(activity.ChannelId,
+                                                                          activity.Conversation.Id,
+                                                                          activity.From.Id);
+
+                var haveGreeting = userData.GetProperty<bool>("HaveGreeting");
+                // Create text for a reply message   
+                StringBuilder strReplyMessage = new StringBuilder();
+                if (haveGreeting == false) 
+                {
+                    strReplyMessage.Append($"Hi, how are you today?");
+                    userData.SetProperty("HaveGreeting", true);
+                }
+                else 
+                {
+                    if (activity.Text.IndexOf("How are you", 0, StringComparison.CurrentCultureIgnoreCase) != -1)
+                    {
+                        strReplyMessage.Append("Great! Thank for asking");
+                    }
+                    else
+                    {
+                        strReplyMessage.Append($"You said: {activity.Text}");
+                    }
+                }
+
+                // Save BotUserData
+                sc.BotState.SetPrivateConversationData(
+                    activity.ChannelId, activity.Conversation.Id, activity.From.Id, userData);
+                // Create a reply message
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                Activity replyMessage = activity.CreateReply(strReplyMessage.ToString());
+                await connector.Conversations.ReplyToActivityAsync(replyMessage);
             }
             else
             {
-                HandleSystemMessage(activity);
+                Activity replyMessage = HandleSystemMessage(activity);
+                if (replyMessage != null)
+                {
+                    ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                    await connector.Conversations.ReplyToActivityAsync(replyMessage);
+                }
             }
+            // Return response
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
-
         private Activity HandleSystemMessage(Activity message)
         {
             if (message.Type == ActivityTypes.DeleteUserData)
