aac3b31caaca28faa9ade08da26466d82a1d711f
Tran Tung <trantung04@gmail.com>
2017-03-21 16:00:22 +0700

    Add publish package

--- HelloWorldBot/Global.asax.cs
+++ HelloWorldBot/Global.asax.cs
@@ -1,8 +1,4 @@
 ﻿using System.Web.Http;
-using Autofac;
-using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Builder.Dialogs.Internals;
-using Microsoft.Bot.Connector;
 
 namespace HelloWorldBot
 {
@@ -11,14 +7,6 @@ namespace HelloWorldBot
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
-
-            var builder = new ContainerBuilder();
-            builder.Register(c => new CachingBotDataStore(c.ResolveKeyed<IBotDataStore<BotData>>(typeof(ConnectorStore))
-                                                          , CachingBotDataStoreConsistencyPolicy.LastWriteWins))
-                   .As<IBotDataStore<BotData>>()
-                   .AsSelf()
-                   .InstancePerLifetimeScope();
-            builder.Update(Conversation.Container);
         }
     }
 }
